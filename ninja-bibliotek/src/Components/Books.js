8b65907e745c2eacf592eabc42cc257225054468
Niklas Johansson <niklasdjohansson@gmail.com>
2022-04-25 14:24:19 +0200

    #2
    #2

---
+++ ninja-bibliotek/src/Components/Books.js
@@ -0,0 +1,21 @@
+import books from '../assets/books.json'
+import { Link } from 'react-router-dom'
+
+function ShowAllBooks() {
+    const allBooks = books;
+    const bookList = allBooks.map((book) => 
+        <li>{book.title}<br/>{book.author}<br/>{book.amount}</li>
+    );
+
+    return (
+        <section>
+            <h2>Alla våra böcker</h2>
+            <ul>
+                {bookList}
+            </ul>
+        </section>
+    )
+
+}
+
+export default ShowAllBooks;
