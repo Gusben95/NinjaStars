7f790fb9552a69b371fa83ff45e6540ea3830e56
Niklas Johansson <niklasdjohansson@gmail.com>
2022-04-26 09:23:21 +0200

    #2
    #2

--- ninja-bibliotek/src/Components/Books.js
+++ ninja-bibliotek/src/Components/Books.js
@@ -1,16 +1,18 @@
 import books from '../assets/books.json'
 import { Link } from 'react-router-dom'
 
+import './Books.css'
+
 function ShowAllBooks() {
     const allBooks = books;
     const bookList = allBooks.map((book) => 
-        <Link to={`/book/${book.id}`}><li>{book.title}<br/>{book.author}<br/><br/></li></Link>
+        <Link to={`/book/${book.id}`}><li className="oneBook">{book.title}<br/>{book.author}<br/></li></Link>
     );
 
     return (
         <section>
             <h2>Alla våra böcker</h2>
-            <ul>
+            <ul className="booksContainer">
                 {bookList}
             </ul>
         </section>
